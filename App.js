f80572883f68525aad504c8af7f0b99b54ad57ce
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-06-30 21:48:36 +0100

    Implemented Temperature

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null)
   const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitsSystem, setUnitsSystem] = useState('metric')
 
 
   useEffect(() =>{
@@ -27,7 +28,7 @@ export default function App() {
     const location = await Location.getCurrentPositionAsync();
     const {latitude, longitude} = location.coords
 
-    const weatherUrl =`${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    const weatherUrl =`${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
 
     const response = await fetch(weatherUrl)
     const result = await response.json()
@@ -38,19 +39,28 @@ export default function App() {
       setCurrentWeather(result.message)
     }
 
-    } catch (error){}
+    } catch (error){
+      setErrorMessage(error.message)
+    }
 }
 
 if(currentWeather){
-  
-}
+  const {main : {temp}} = currentWeather
 
   return (
     <View style={styles.container}>
-      <Text>Hello from React Natives</Text>
+      <Text>{temp}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+  } else{
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 
